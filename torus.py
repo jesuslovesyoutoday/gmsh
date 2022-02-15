ee7a9c959a65f8eb622bb46eb2805a85181054a0
jesuslovesyoutoday <jesuslovesyoutoday@yadex.ru>
2022-02-15 18:49:42 +0300

    torus upgraded

--- torus.py
+++ torus.py
@@ -5,9 +5,19 @@ import math
 gmsh.initialize()
 
 gmsh.model.add('torus')
+
 gmsh.model.occ.addTorus(0, 0, 0, 20, 10)
 
+gmsh.model.occ.addPlaneSurface([1], 2)
+gmsh.model.occ.addThickSolid(1, [2], offset = 3)
+
 gmsh.model.occ.synchronize()
+
+f = gmsh.model.mesh.field.add("MathEval")
+
+gmsh.model.mesh.field.setString(f, "F", "1")
+gmsh.model.mesh.field.setAsBackgroundMesh(f)
+
 gmsh.model.mesh.generate(2)
 
 if '-nopopup' not in sys.argv:
